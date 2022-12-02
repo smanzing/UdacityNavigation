13e4ffbbaf5c2221c502f687c339841af6fec664
Stefanie Manzinger <stef.manz@kabelmail.de>
2022-12-02 12:01:31 +0100

    added Udacity repo as submodule, added library rl_lib to solve task, and added Notebook for training the agent

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # UdacityNavigation
+
+## Getting Started
+
+Follow the instructions below to set up your python environment to run the code in this repository.
+Note that the installation was only tested for __Mac__.
+
+1. Initialize the git submodules in the root folder of this repository. 
+
+	```bash
+	git submodule init
+	git submodule update
+	```
+ 
+2. Create (and activate) a new conda environment with Python 3.6.
+
+	```bash
+	conda create --name drlnd python=3.6
+	source activate drlnd
+	```
+	
+3. Install the base Gym library and the **box2d** environment group:
+
+	```bash
+	pip install gym
+	pip install Box2D gym
+	```
+
+4. Navigate to the `external/Value-based-methods/python/` folder.  Then, install several dependencies.
+
+    ```bash
+    cd external/Value-based-methods/python
+    pip install .
+    ```
+    **Note**: If an error appears during installation regarding the torch version, please remove the torch version 0.4.0 in
+    external/Value-based-methods/python/requirements.txt and try again.
+
+5. Create an [IPython kernel](http://ipython.readthedocs.io/en/stable/install/kernel_install.html) for the `drlnd` environment.  
+    
+    ```bash
+    python -m ipykernel install --user --name drlnd --display-name "drlnd"
+    ```
+    
+    **Note**: Before running code in a notebook, change the kernel to match the `drlnd` environment by using the drop-down `Kernel` menu. 
+
+6. Download the [Unity environment](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana.app.zip)  in the **root folder** of this repository.
+
+    
---
+++ rl_lib/training.py
@@ -0,0 +1,69 @@
+import torch
+import numpy as np
+import random
+from collections import deque
+
+from rl_lib.model import QNetwork
+from rl_lib.agent import Agent
+
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def dqn(env, brain_name, agent, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995, min_avg_score=13.0, continue_learning=False):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    min_score_achieved = False
+    for i_episode in range(1, n_episodes+1):
+        # reset the environment
+        env_info = env.reset(train_mode=True)[brain_name]
+        # get current state
+        state = env_info.vector_observations[0]
+        # initialize score
+        score = 0
+        for t in range(max_t):
+            # select action
+            action = agent.act(state, eps)
+            # simulate
+            env_info = env.step(action)[brain_name]
+            # get next state, reward, and check if episode has finished
+            next_state = env_info.vector_observations[0]
+            reward = env_info.rewards[0] 
+            done = env_info.local_done[0]
+            
+            agent.step(state, action, reward, next_state, done)
+            
+            # update state and reward
+            state = next_state
+            score += reward
+            
+            if done:
+                break 
+        
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=min_avg_score and not min_score_achieved:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint_min_score_achieved.pth')
+            min_score_achieved = True
+            if not continue_learning:
+                break
+    torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+    return scores
