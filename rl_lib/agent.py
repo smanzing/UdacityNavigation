583babde922713da04f42d071e6be963333af13f
Stefanie Manzinger <stef.manz@kabelmail.de>
2023-01-03 13:21:48 +0100

    fixed a few bugs, prioritized sampling should work now

--- rl_lib/agent.py
+++ rl_lib/agent.py
@@ -117,7 +117,7 @@ class Agent:
         """Update value parameters using a sampled batch of experience tuples."""
 
         if self.t_step_local == 0:
-            states, actions, rewards, next_states, dones, batch_indices = self.replay_buffer.sample()
+            states, actions, rewards, next_states, dones, batch_indices, normalized_importance_sampling_weigths = self.replay_buffer.sample()
 
             delta, q_targets, q_expected = self.get_temporal_difference_error(
                 states, actions, next_states, rewards, dones)
@@ -125,7 +125,8 @@ class Agent:
             # update priorities in replay buffer
             self.replay_buffer.update_priorities(batch_indices, np.abs(delta.detach().numpy()))
             # Compute loss
-            loss = F.mse_loss(q_expected, q_targets)
+            # loss = F.mse_loss(q_expected, q_targets)
+            loss = torch.mean((normalized_importance_sampling_weigths * delta) ** 2)
             # Minimize the loss
             self.optimizer.zero_grad()
             loss.backward()
--- rl_lib/replay.py
+++ rl_lib/replay.py
@@ -9,7 +9,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, action_size, buffer_size, batch_size, random_seed):
+    def __init__(self, action_size, buffer_size, batch_size, random_seed, alpha, beta):
         """
         @param action_size: dimension of each action
         @param buffer_size: maximum size of buffer
@@ -24,11 +24,13 @@ class ReplayBuffer:
                                      field_names=["state", "action", "reward", "next_state", "done", "priority"])
         self.seed = random.seed(random_seed)
 
+        self.alpha = alpha
+        self.beta = beta
+
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
         if len(self.memory) > self.batch_size:
             max_priority = max([experience.priority for experience in self.memory])
-            print(max_priority)
         else:
             max_priority = 1.0
         e = self.experience(state, action, reward, next_state, done, max_priority)
@@ -38,9 +40,7 @@ class ReplayBuffer:
         """Randomly sample a batch of experiences from memory."""
         # experiences = random.choices(self.memory, k=self.batch_size)
         priorities = np.array([experience.priority for experience in self.memory]).astype('float64')
-        probabilites = priorities / np.sum(priorities)
-        print(probabilites.shape)
-        print(probabilites)
+        probabilites = priorities ** self.alpha / np.sum(priorities ** self.alpha)
         batch_indices = np.random.choice(len(self.memory), size=self.batch_size, p=probabilites)
 
         states = torch.from_numpy(
@@ -56,11 +56,18 @@ class ReplayBuffer:
             np.vstack([self.memory[i].done for i in batch_indices if self.memory[i] is not None]).astype(
                 np.uint8)).float().to(
             device)
-        return states, actions, rewards, next_states, dones, batch_indices
+        importance_sampling_weights = torch.from_numpy(
+            np.vstack([(len(self.memory) * probabilites[i]) ** (-self.beta) for i in batch_indices if
+                       self.memory[i] is not None])).float().to(
+                device)
+        normalized_importance_sampling_weigths = importance_sampling_weights / max(importance_sampling_weights)
+        return states, actions, rewards, next_states, dones, batch_indices, normalized_importance_sampling_weigths
 
     def update_priorities(self, batch_indices, updated_priorities):
         for i, priority in zip(batch_indices, updated_priorities):
-            self.memory[i] = self.memory[i]._replace(priority=priority)
+            # add offset of 1e-6 to priority to prevent experiences from being starved for selection due
+            # to zero TD error
+            self.memory[i] = self.memory[i]._replace(priority=priority.item() + 1e-6)
 
     def __len__(self):
         """Return the current size of internal memory."""
